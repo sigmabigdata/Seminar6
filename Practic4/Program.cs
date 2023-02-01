470b8c4c26ec5063618aa02086ee30a4ee73c2f5
anatoli dyrul <dyrulan@gmail.com>
2023-02-01 23:33:09 +0300

    all practic complite

--- Practic4/Program.cs
+++ Practic4/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] Fibonachi(int number)
+{
+    int[] array = new int[number];
+    if (number == 1)
+    return new int[1] {0};
+
+    if (number == 2)
+    return new int[2] {0, 1};
+    array [0] = 0;
+    array [1] = 1;
+    for (int i = 2; i < number; i++)
+    {
+        array[i] = array[i-1] + array[i-2]; 
+    }
+    return array;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = Fibonachi(number);
+Console.WriteLine(string.Join(", ", array));
---
+++ Practic5/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] InitArray(int dimention)
+
+
+{
+    int[] result = new int[dimention];
+    Random rnd = new Random();
+
+    for (int i = 0; i < dimention; i++)
+    {
+        result[i] = rnd.Next(1,10);
+    }
+
+    return result;
+}
+
+int [] CopyArray(int[] array)
+{
+    int[] arr = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        arr[i] = array[i];
+    }
+    return arr;
+}
+int dimension = 5;
+int[] array = InitArray(dimension);
+Console.WriteLine(string.Join(", ", array ));
+int [] arr = CopyArray(array);
+Console.WriteLine(string.Join(", ", arr ));
