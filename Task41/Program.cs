e599a81e929962024c5ad658545f7b648a805e6d
anatoli dyrul <dyrulan@gmail.com>
2023-02-03 00:34:05 +0300

    task 43 complit

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,1 +1,66 @@
 ﻿
+/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+
+вводим через один Enter (разбиваем через Split())
+*/
+
+Console.WriteLine("Введите числа: ");
+int[] InitArray(int dimention)
+
+
+{
+    int[] result = new int[dimention];
+    
+
+    for (int i = 0; i < dimention; i++)
+    {
+        result[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+int[] startArray = InitArray(5);
+
+void SplitArray(int [] startArray)
+{
+int[] arr = startArray; 
+string[] massStr = new string [arr.Length];
+for(int i = 0; i < arr.Length; i++)
+    {
+        massStr[i] = arr[i].ToString();
+    }
+
+}
+
+void GetMoreZero(int[] arr)
+{
+int count = 0;
+ 
+ for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+  }
+    Console.WriteLine($"Вы ввели {count} чисел больше 0.");
+}
+
+PrintArray(startArray);
+SplitArray(startArray);
+GetMoreZero(startArray);
